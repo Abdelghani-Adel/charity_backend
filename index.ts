e03a76f54d58bd2c5dcd0bf5e7398fff7ddb5a1f
abdelghany <abdelghany@rockettravelsystem.com>
2024-06-24 12:19:27 +0300

    Edit port

--- index.ts
+++ index.ts
@@ -1,25 +1,16 @@
 import express from "express";
+import Logger from "./startup/logging";
 const app = express();
 
-app.get("/ee", (req, res) => res.send("Express on Vercel"));
+process.on("uncaughtException", (err) => {
+  Logger.error(`Uncaught Exception: ${err.message}`);
+});
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
+require("./startup/config")();
+require("./startup/db").connect();
+require("./startup/routes")(app);
 
-module.exports = app;
+const port = 3000;
+const server = app.listen(port, () => Logger.info(`Listening on port ${port}...`));
 
-// import express from "express";
-// import Logger from "./startup/logging";
-// const app = express();
-
-// process.on("uncaughtException", (err) => {
-//   Logger.error(`Uncaught Exception: ${err.message}`);
-// });
-
-// require("./startup/config")();
-// require("./startup/db").connect();
-// require("./startup/routes")(app);
-
-// const port = 3000;
-// const server = app.listen(port, () => Logger.info(`Listening on port ${port}...`));
-
-// module.exports = server;
+module.exports = server;
