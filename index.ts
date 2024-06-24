8822338e6a6565d242b325501e2c58cf35307816
abdelghany <abdelghany@rockettravelsystem.com>
2024-06-24 12:10:56 +0300

    Edit port

--- index.ts
+++ index.ts
@@ -10,7 +10,7 @@ require("./startup/config")();
 require("./startup/db").connect();
 require("./startup/routes")(app);
 
-const port = 8000;
+const port = 3000;
 const server = app.listen(port, () => Logger.info(`Listening on port ${port}...`));
 
 module.exports = server;
