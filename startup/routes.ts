c5ba90238bed1874401d94f42abe9232090c4a28
abdelghany <abdelghany@rockettravelsystem.com>
2024-06-24 13:03:06 +0300

    Edit port

--- startup/routes.ts
+++ startup/routes.ts
@@ -1,10 +1,12 @@
 import express, { Express } from "express";
 import auth from "../routes/auth";
+import uptimerobot from "../routes/uptimerobot";
 import indigent from "../routes/indigent";
 import { validateToken } from "../middleware/auth";
 
 module.exports = function (app: Express) {
   app.use(express.json());
+  app.use("/uptimerobot", uptimerobot);
   app.use("/api/auth", auth);
   app.use(validateToken);
   app.use("/api/indigent", indigent);
