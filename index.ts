6ff586972f0630666de945e3cf56513e764c3584
abdelghany <abdelghany@rockettravelsystem.com>
2024-06-26 11:53:36 +0300

    keep db connection alive

--- index.ts
+++ index.ts
@@ -1,19 +1,28 @@
 import express from "express";
-import Logger from "./startup/logging";
 const app = express();
 
-process.on("uncaughtException", (err) => {
-  Logger.error(`Uncaught Exception: ${err.message}`);
-});
+app.get("/", (req, res) => res.send("Express on Vercel"));
 
-require("./startup/config")();
-require("./startup/db").connect();
-require("./startup/routes")(app);
+app.listen(3000, () => console.log("Server ready on port 3000."));
 
-const port = process.env.PORT || 3030;
-if (require.main === module) {
-  // Ensure this is the main module
-  app.listen(port, () => Logger.info(`Listening on port ${port}...`));
-}
+module.exports = app;
 
-export default app;
+// import express from "express";
+// import Logger from "./startup/logging";
+// const app = express();
+
+// process.on("uncaughtException", (err) => {
+//   Logger.error(`Uncaught Exception: ${err.message}`);
+// });
+
+// require("./startup/config")();
+// require("./startup/db").connect();
+// require("./startup/routes")(app);
+
+// const port = process.env.PORT || 3030;
+// if (require.main === module) {
+//   // Ensure this is the main module
+//   app.listen(port, () => Logger.info(`Listening on port ${port}...`));
+// }
+
+// export default app;
