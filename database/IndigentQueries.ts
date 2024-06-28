704d918a07d016f01aeac03dd6e7e67f75bb95e2
abdelghany <abdelghany@rockettravelsystem.com>
2024-06-28 12:11:24 +0300

    update

--- database/IndigentQueries.ts
+++ database/IndigentQueries.ts
@@ -1,6 +1,7 @@
 import { executeQuery } from "../startup/db";
 import IQR_GetAllIndigents from "../types/query_results/IQR_GetAllIndigents";
 import { IQR_GetIndigentAids } from "../types/query_results/IQR_GetIndigentAids";
+import { IQR_GetIndigentInfo } from "../types/query_results/IQR_GetIndigentInfo";
 
 export async function getIndigentList(): Promise<IQR_GetAllIndigents | null> {
   const query = `SELECT * FROM get_indigents_with_details();`;
@@ -17,8 +18,8 @@ export async function getIndigentAids(
   return result;
 }
 
-// export async function getIndigentDetails(indigent_id: string): Promise<IQR_GetAllIndigents | null> {
-//   const query = `SELECT * FROM get_indigents_with_details();`;
-//   const result: IQR_GetAllIndigents = await executeQuery(query);
-//   return result;
-// }
+export async function getIndigentInfo(indigent_id: string): Promise<IQR_GetIndigentInfo | null> {
+  const query = `SELECT * FROM get_indigent_info(${indigent_id});`;
+  const result: IQR_GetIndigentInfo[] = await executeQuery(query);
+  return result ? result[0] : null;
+}
--- types/api_responses/IApiRes_GetIndigentDetails.d.ts
+++ types/api_responses/IApiRes_GetIndigentDetails.d.ts
@@ -1,6 +1,7 @@
 import { IQR_GetIndigentAids } from "../query_results/IQR_GetIndigentAids";
+import { IQR_GetIndigentInfo } from "../query_results/IQR_GetIndigentInfo";
 
 export interface IApiRes_GetIndigentDetails {
-  info: "";
+  info: IQR_GetIndigentInfo | null;
   aids: IQR_GetIndigentAids[] | [] | null;
 }
