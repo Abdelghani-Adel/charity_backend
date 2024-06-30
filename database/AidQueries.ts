68429dd22e43caeed86cdb82fd4de0d13e221eab
abdelghany <abdelghany@rockettravelsystem.com>
2024-06-30 11:45:51 +0300

    update

--- database/AidQueries.ts
+++ database/AidQueries.ts
@@ -8,7 +8,7 @@ export async function db_insertNewAid(
   desc: string,
   isPublic: boolean
 ): Promise<IApiRes_InsertAid | null> {
-  const query = `SELECT insert_aid(${org_id}, ${ind_id}, ${aid_type_id}, ${desc}, ${isPublic});`;
+  const query = `SELECT insert_aid(${org_id}, ${ind_id}, ${aid_type_id}, '${desc}', ${isPublic});`;
   const result: IApiRes_InsertAid[] | null = await executeQuery(query);
   return result ? result[0] : null;
 }
