41e9143c14236dd5c9fe8e8087a91de0455017f3
abdelghany <abdelghany@rockettravelsystem.com>
2024-09-02 13:20:39 +0300

    Changed the group api handlers to POST

--- types/api_responses/ApiRes_GetGroupInfo.d.ts
+++ types/api_responses/ApiRes_GetGroupInfo.d.ts
@@ -2,4 +2,4 @@ import IQR_GetGroupInfo from "../query_results/IQR_GetGroupInfo";
 
 type IApiRes_GetGroupInfo = IQR_GetGroupInfo | [];
 
-export default IApiRes_GetAllIndigents;
+export default IApiRes_GetGroupInfo;
--- types/api_responses/IApiRes_GetOrgGroups.d.ts
+++ types/api_responses/IApiRes_GetOrgGroups.d.ts
@@ -2,4 +2,4 @@ import IQR_GetOrgGroups from "../query_results/IQR_GetOrgGroups";
 
 type IApiRes_GetOrgGroups = IQR_GetOrgGroups | [];
 
-export default IApiRes_GetAllIndigents;
+export default IApiRes_GetOrgGroups;
