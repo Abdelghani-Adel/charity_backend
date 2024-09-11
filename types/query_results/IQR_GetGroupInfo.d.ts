03c4660f44219785e760c67724ec8016829a818d
abdelghany <abdelghany@rockettravelsystem.com>
2024-09-11 17:20:10 +0300

    update

--- types/query_results/IQR_GetGroupInfo.d.ts
+++ types/query_results/IQR_GetGroupInfo.d.ts
@@ -1,7 +1,16 @@
 // Interface for each indigent in the group
 export interface IQR_Indigent {
-  id: number;
-  name: string;
+  indigent_id: number;
+  national_id: string;
+  indigent_name: string;
+  phone: string | null;
+  kids: number | null;
+  indigency_type_name: string;
+  governorate_name: string;
+  city_name: string;
+  district_name: string;
+  address: string | null;
+  is_active: boolean;
 }
 
 // Interface for the response of the `get_group_details` function
